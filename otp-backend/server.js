79180b141cf92d20947c5c5de754b96460af401f
Roopkumar213 <roopkumar3244@gmail.com>
2025-08-28 20:21:19 +0530

    final server side commit for Authentication

--- otp-backend/server.js
+++ otp-backend/server.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
+
 const app = express();
 
 // Middleware
@@ -496,7 +497,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Start server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
